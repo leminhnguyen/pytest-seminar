7e2f1d4c770350d30f77a8cfc93ff41c32708463
nguyenlm-vbee <nguyenlm@vbee.vn>
2021-11-25 16:17:11 +0700

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# PyTest Talk
+ 
+![pytest_passing_tests](resources/pytest_demo_running.gif)
+
+## Installation
+
+```sh
+$ pip install -r requirements.txt
+```
+
+## Run Unittest Demo
+
+```sh
+$ pytest pytest-demo/demo-tests
+```
---
+++ demo-tests/test_04.py
@@ -0,0 +1,13 @@
+import time 
+import random
+
+def test_func_04(load_review_rating):
+    ## setup ##
+    data, expected = load_review_rating
+
+    ## call test fuction ##
+    actual = int(data[:-1])
+    time.sleep(abs(random.random()-0.5))
+
+    ## assertion ##
+    assert  actual > expected
